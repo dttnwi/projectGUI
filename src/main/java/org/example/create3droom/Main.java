de7cdac91e5d35b6c7247beca68d9c286560f33d
Sofya Vinogradova <sofyavinogradowa5@gmail.com>
2025-06-09 21:56:43 +0300

    добавлены все модели мебели

--- src/main/java/org/example/create3droom/Main.java
+++ src/main/java/org/example/create3droom/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
+//favf
 public class Main extends Application {
 
     @Override
